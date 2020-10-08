45b896cbb6362bfc43c9555f6def7ff1f632a7f4
KenyAmbrocio1308 <72556120+KenyAmbrocio1308@users.noreply.github.com>
2020-10-08 06:14:57 -0600

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,205 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="es-ES" dir="ltr">
+	<head>
+		<title>Buscar - Tienda y Libreria &quot;Angeles&quot;</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Keny Ambrocio" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 14.0.4.3 - www.websitex5.com" />
+		<meta property="og:locale" content="es" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://abarrotesangeles.com/imsearch.php" />
+		<meta property="og:title" content="Buscar" />
+		<meta property="og:site_name" content="Tienda y Libreria &quot;Angeles&quot;" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?14-0-4-3" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?14-0-4-3" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?14-0-4-3" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?14-0-4-3" media="screen" />
+		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?14-0-4-3-637377268766251599" media="screen,print" />
+		<script type="text/javascript" src="res/jquery.js?14-0-4-3"></script>
+		<script type="text/javascript" src="res/x5engine.js?14-0-4-3" data-files-version="14-0-4-3"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('El Explorador que estás usando no es compatible con las funciones requeridas para mostrar este Sitio web.','El Navegador que estás utilizando podría no ser compatible con las funciones requeridas para poder ver este Sitio web.','[1]Actualiza tu explorador [/1] o [2]continuar de todos modos[/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<div id="imHeader">
+						<h1 class="imHidden">Buscar - Tienda y Libreria &quot;Angeles&quot;</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_09_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_9" class="" > <div id="imHeader_imCellStyleGraphics_9"></div><div id="imHeader_imCellStyle_9" ><div id="imHeader_imMenuObject_09"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+INICIO		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imLevel"><div class="label-wrapper"><div class="label-inner-wrapper"><span class="label">SERVICIOS</span></div></div><ul data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/abarrotes.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="abarrotes.html">
+ABARROTES		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/libreria.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="libreria.html">
+LIBRERIA		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/internet.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="internet.html">
+INTERNET		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/farmacia.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="farmacia.html">
+FARMACIA		</a>
+</div>
+</div>
+	</li></ul></li><li class="imMnMnMiddle imPage" data-link-paths="/ubicacion.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="ubicacion.html">
+UBICACION		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/contacto.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="contacto.html">
+CONTACTO		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_09',1000)});
+$(function () {$('#imHeader_imMenuObject_09 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_09-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_09-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imHeader_imObjectImage_10_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_10" class="" > <div id="imHeader_imCellStyleGraphics_10"></div><div id="imHeader_imCellStyle_10" ><img id="imHeader_imObjectImage_10" src="images/angelidkproject--2-.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectTitle_11_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_11" class="" > <div id="imHeader_imCellStyleGraphics_11"></div><div id="imHeader_imCellStyle_11" ><div id="imHeader_imObjectTitle_11"><span id ="imHeader_imObjectTitle_11_text" >TEL: 3241-4295</span > </div></div></div></div><div id="imHeader_imObjectTitle_12_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_12" class="" > <div id="imHeader_imCellStyleGraphics_12"></div><div id="imHeader_imCellStyle_12" ><div id="imHeader_imObjectTitle_12"><span id ="imHeader_imObjectTitle_12_text" >abarrotesangeles@gmail.com</span > </div></div></div></div></div>
+					</div>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_1" class="" > <div id="imStickyBar_imCellStyleGraphics_1"></div><div id="imStickyBar_imCellStyle_1" ><div id="imStickyBar_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+INICIO		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imLevel"><div class="label-wrapper"><div class="label-inner-wrapper"><span class="label">SERVICIOS</span></div></div><ul data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/abarrotes.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="abarrotes.html">
+ABARROTES		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/libreria.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="libreria.html">
+LIBRERIA		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/internet.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="internet.html">
+INTERNET		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/farmacia.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="farmacia.html">
+FARMACIA		</a>
+</div>
+</div>
+	</li></ul></li><li class="imMnMnMiddle imPage" data-link-paths="/ubicacion.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="ubicacion.html">
+UBICACION		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/contacto.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="contacto.html">
+CONTACTO		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_01',1000)});
+$(function () {$('#imStickyBar_imMenuObject_01 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imStickyBar_imObjectImage_02_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_2" class="" > <div id="imStickyBar_imCellStyleGraphics_2"></div><div id="imStickyBar_imCellStyle_2" ><img id="imStickyBar_imObjectImage_02" src="images/angelidkproject--2-_57palcux.png" title="" alt="" /></div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<div id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Resultados de búsqueda</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+							<div class="imClear"></div>
+						</div>
+					</div>
+					<div id="imFooter">
+						<div id="imFooterObjects"><div id="imFooter_imObjectTitle_01_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_1" class="" > <div id="imFooter_imCellStyleGraphics_1"></div><div id="imFooter_imCellStyle_1" ><div id="imFooter_imObjectTitle_01"><span id ="imFooter_imObjectTitle_01_text" >2020 Creado con WebSite - Todos los derechos son reservados</span > </div></div></div></div></div>
+					</div>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Lea esta página de nuevo">Regreso al contenido</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Para utilizar este sitio tienes que habilitar JavaScript.</div></noscript>
+	</body>
+</html>
